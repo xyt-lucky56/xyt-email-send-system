14fedada5771d43221bea0a5994d5b5ce89fa0b8
liumenghui <793540604@qq.com>
2019-09-20 16:29:34 +0800

    tijiao

--- src/main/java/com/xyt/sms/controller/CookieController.java
+++ src/main/java/com/xyt/sms/controller/CookieController.java
@@ -2,6 +2,7 @@ package com.xyt.sms.controller;
 
 import com.xyt.utils.RandomCodeUtil;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,6 +21,7 @@ import java.util.Map;
  */
 @RestController
 public class CookieController {
+    @CrossOrigin
     @GetMapping("/getCookieCode")
     public Map<String,Object> getCookieCode(HttpServletRequest request, HttpServletResponse response){
         Map map=new HashMap();
@@ -27,12 +30,29 @@ public class CookieController {
             map.put("error","请传递手机号!");
         }else {
             String randomCode = RandomCodeUtil.getRandomCode();
+            String myOrigin = request.getHeader("origin");
+            myOrigin = getDoMain(myOrigin);
             Cookie cookie=new Cookie(phoneNumber,randomCode);
-            cookie.setMaxAge(2);
-            cookie.setPath(request.getRequestURI());
+            cookie.setMaxAge(2*60);
+            cookie.setPath("/");
+            cookie.setDomain(myOrigin);
+            cookie.setHttpOnly(false);
             response.addCookie(cookie);
-            map.put("success","成功");
+            map.put("超时时间",2);
         }
         return map;
+    }
+    private String getDoMain(String myOrigin){
+        String newOrigin = myOrigin.replace("https://","")
+                .replace("http://","");
+        int i = newOrigin.indexOf(":");
+        if (i > -1) {
+            newOrigin = newOrigin.substring(0,i);
+        }
+        int index = newOrigin.indexOf(".");
+        if (index > -1) {
+            newOrigin = newOrigin.substring(index + 1);
+        }
+        return newOrigin;
     }
 }
